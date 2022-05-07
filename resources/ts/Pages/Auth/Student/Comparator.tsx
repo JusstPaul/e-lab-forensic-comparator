430ccb1a0a16fe96e649028f1ad1f0b44c7a7a44
Justine Paul Lacap <lacappaul21@gmail.com>
2022-05-07 23:36:40 +0800

    Begin replacing image slider with canvas

--- resources/ts/Pages/Auth/Student/Comparator.tsx
+++ resources/ts/Pages/Auth/Student/Comparator.tsx
@@ -1,11 +1,7 @@
-import { FC, useState, useCallback } from 'react'
+// NOTE: Whoever reads this code, just don't.
+import { FC, useState, useCallback, useEffect, createRef } from 'react'
 import { Inertia } from '@inertiajs/inertia'
 import { Link, usePage } from '@inertiajs/inertia-react'
-import {
-  ReactCompareSlider,
-  ReactCompareSliderImage,
-  ReactCompareSliderHandle,
-} from 'react-compare-slider'
 import {
   ArrowDownIcon,
   ArrowLeftIcon,
@@ -21,7 +17,6 @@ import CheckBox from '@/Components/CheckBox'
 import { ComparatorState } from './ActivityAnswer'
 import s3Client, { S3PageProps, getFileURL } from '@/Lib/s3'
 import Editor from '@/Components/Editor'
-import cloneDeep from 'lodash.clonedeep'
 
 type Props = {
   id: string
@@ -36,6 +31,9 @@ const Comparator: FC<Props> = ({ id, activity_id, answer_index, state }) => {
   const client = s3Client(_aws)
 
   const [comparator, setComparator] = useState(state.answer)
+
+  const [leftSrc, setLeftSrc] = useState('')
+  const [rightSrc, setRightSrc] = useState('')
 
   const [imageClickMode, setImageClickMode] = useState<'set' | 'preview'>('set')
   const [currentImage, setCurrentImage] = useState(0)
@@ -55,7 +53,125 @@ const Comparator: FC<Props> = ({ id, activity_id, answer_index, state }) => {
     setIsViewerOpen(false)
   }, [])
 
+  const containerRef = createRef<HTMLDivElement>()
+  const canvasRef = createRef<HTMLCanvasElement>()
+  const [sliderDimensions, setSliderDimensions] = useState({
+    width: 0,
+    height: 0,
+  })
+  const [sliderPos, setSliderPos] = useState(0)
+
   const [imageSide, setImageSide] = useState<'left' | 'right'>('left')
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const { width, height } = containerRef.current.getBoundingClientRect()
+      setSliderPos(Math.round(width / 2))
+
+      setSliderDimensions({ width, height })
+    }
+  }, [])
+
+  useEffect(() => {
+    setLeftSrc(
+      getFileURL(
+        client,
+        _aws.bucket,
+        comparator.images[comparator.current.left]
+      )
+    )
+    setRightSrc(
+      getFileURL(
+        client,
+        _aws.bucket,
+        comparator.images[comparator.current.right]
+      )
+    )
+  }, [comparator])
+
+  const [isSliderMoving, setIsSliderMoving] = useState(false)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const { width, height } = containerRef.current.getBoundingClientRect()
+
+      if (canvasRef.current) {
+        const { current } = canvasRef
+
+        const ctx = current.getContext('2d')
+        if (ctx) {
+          const covered = sliderPos / width
+
+          const leftImg = new Image()
+          leftImg.src = leftSrc
+          leftImg.onload = () => {
+            ctx.drawImage(
+              leftImg,
+              0,
+              0,
+              leftImg.width * covered,
+              leftImg.height,
+              0,
+              0,
+              sliderPos,
+              height
+            )
+          }
+
+          const rightImg = new Image()
+          rightImg.src = rightSrc
+          rightImg.onload = () => {
+            ctx.drawImage(
+              rightImg,
+              rightImg.width * covered,
+              0,
+              rightImg.width,
+              rightImg.height,
+              sliderPos,
+              0,
+              width,
+              height
+            )
+
+            // Draw slider
+            ctx.beginPath()
+            ctx.strokeStyle = '#ffffff'
+            ctx.lineWidth = 5
+            ctx.lineJoin = 'miter'
+            ctx.moveTo(sliderPos, 0)
+            ctx.lineTo(sliderPos, height)
+            ctx.stroke()
+            ctx.closePath()
+          }
+        }
+      }
+    }
+  }, [containerRef, canvasRef])
+
+  // TODO: Add annotations
+  const Scene = () => {
+    return (
+      <canvas
+        ref={canvasRef}
+        className="bg-gray-300 border border-dark rounded overflow-hidden shadow-sm"
+        width={sliderDimensions.width}
+        height={sliderDimensions.height}
+        onMouseDown={(event) => {
+          const pos = event.clientX - (event.target as any).offsetLeft
+          if (pos >= sliderPos - 2 && pos <= sliderPos + 2) {
+            setIsSliderMoving(true)
+          }
+        }}
+        onMouseUp={() => setIsSliderMoving(false)}
+        onMouseLeave={() => setIsSliderMoving(false)}
+        onMouseMove={(event) => {
+          if (isSliderMoving) {
+            setSliderPos(sliderPos + event.movementX)
+          }
+        }}
+      />
+    )
+  }
 
   return (
     <Class id={id} mode={3}>
@@ -90,98 +206,12 @@ const Comparator: FC<Props> = ({ id, activity_id, answer_index, state }) => {
         </div>
       </div>
       <div className="flex mt-4 px-2">
-        <div className="flex-grow">
-          <div className="border border-dark rounded bg-slate-200 overflow-hidden shadow-sm mb-4 w-3/6 mx-auto">
-            <ReactCompareSlider
-              itemOne={
-                <ReactCompareSliderImage
-                  src={getFileURL(
-                    client,
-                    _aws.bucket,
-                    comparator.images[comparator.current.left]
-                  )}
-                  style={{ position: 'relative', ...comparator.styles.left }}
-                />
-              }
-              itemTwo={
-                <ReactCompareSliderImage
-                  src={getFileURL(
-                    client,
-                    _aws.bucket,
-                    comparator.images[comparator.current.right]
-                  )}
-                  style={{ position: 'relative', ...comparator.styles.right }}
-                />
-              }
-              onlyHandleDraggable={true}
-              onPositionChange={(position) =>
-                setComparator({ ...comparator, position: position })
-              }
-              handle={
-                <ReactCompareSliderHandle
-                  buttonStyle={{ display: 'none' }}
-                  linesStyle={{ height: '100%', width: 3 }}
-                />
-              }
-              position={comparator.position}
-            />
-          </div>
-
-          <div className="flex justify-center gap-2 overflow-y-auto px-4 py-2">
-            {comparator.images.map((value, index) => (
-              <img
-                key={index}
-                className={
-                  'w-16 h-16 rounded-md cursor-pointer ' +
-                  ((index == comparator.current.left ||
-                    index == comparator.current.right) &&
-                    'border-2 border-primary')
-                }
-                src={getFileURL(client, _aws.bucket, value)}
-                onClick={() => {
-                  console.log(comparator)
-                  switch (imageClickMode) {
-                    case 'preview':
-                      openImageViewer(index)
-                      break
-                    case 'set':
-                      if (comparator.select_mode == 'left') {
-                        setComparator({
-                          ...comparator,
-                          current: {
-                            ...comparator.current,
-                            left: index,
-                          },
-                        })
-                      } else {
-                        setComparator({
-                          ...comparator,
-                          current: {
-                            ...comparator.current,
-                            right: index,
-                          },
-                        })
-                      }
-                  }
-                }}
-              />
-            ))}
-          </div>
-
-          <div className="px-4 py-2 w-3/6 mx-auto">
-            <Editor
-              name="essay"
-              setContents={comparator.essay ? comparator.essay : ''}
-              onChange={(content) => {
-                const nComparator = {
-                  ...cloneDeep(comparator),
-                  essay: content,
-                } as ComparatorState
-                nComparator.essay = content
-                console.log(nComparator)
-                setComparator(nComparator)
-              }}
-            />
+        <div className="flex-grow overflow-hidden px-8">
+          <div
+            ref={containerRef}
+            className="h-full md:w-6/12 mx-auto px-2 md:px-4 overflow-hidden"
+          >
+            <Scene />
           </div>
         </div>
 
@@ -782,6 +812,72 @@ const Comparator: FC<Props> = ({ id, activity_id, answer_index, state }) => {
         </div>
       </div>
 
+      <div className="flex justify-center gap-2 overflow-y-auto px-4 py-2">
+        {comparator.images.map((value, index) => (
+          <img
+            key={index}
+            className={
+              'w-16 h-16 rounded-md cursor-pointer ' +
+              ((index == comparator.current.left ||
+                index == comparator.current.right) &&
+                'border-2 border-primary')
+            }
+            src={getFileURL(client, _aws.bucket, value)}
+            onClick={() => {
+              console.log(comparator)
+              switch (imageClickMode) {
+                case 'preview':
+                  openImageViewer(index)
+                  break
+                case 'set':
+                  if (comparator.select_mode == 'left') {
+                    setComparator({
+                      ...comparator,
+                      current: {
+                        ...comparator.current,
+                        left: index,
+                      },
+                    })
+
+                    /*                     const nLeftImg = leftImg
+                    nLeftImg.src = getFileURL(
+                      client,
+                      _aws.bucket,
+                      comparator.images[index]
+                    )
+                    setLeftImg(nLeftImg) */
+                  } else {
+                    setComparator({
+                      ...comparator,
+                      current: {
+                        ...comparator.current,
+                        right: index,
+                      },
+                    })
+
+                    /*                     const nRightImg = rightImg
+                    nRightImg.src = getFileURL(
+                      client,
+                      _aws.bucket,
+                      comparator.images[index]
+                    )
+                    setRightImg(nRightImg) */
+                  }
+              }
+            }}
+          />
+        ))}
+      </div>
+
+      <div className="px-4 py-2 w-3/6 mx-auto">
+        <Editor
+          name="essay"
+          setContents={comparator.essay ? comparator.essay : ''}
+          onChange={(content) => {
+            setComparator({ ...comparator, essay: content })
+          }}
+        />
+      </div>
       {isViewerOpen ? (
         <ImageViewer
           src={images}
